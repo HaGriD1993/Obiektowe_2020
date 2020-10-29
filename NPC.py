2b3b0656d1d30b232403fe19b6b16d1089efd640
HaGriD1993 <57565779+HaGriD1993@users.noreply.github.com>
2020-10-29 19:37:57 +0100

    Update NPC.py
    poprawki #2

--- NPC.py
+++ NPC.py
@@ -1,5 +1,5 @@
 import random
-#import time
+import time
 
 
 class NPC:
@@ -9,24 +9,52 @@ class NPC:
         self.specjanosc = specjanosc
 
     def info(self):
-        return "Witaj nazywam się: " + self.imie, "pracuje jako: "+self.specjanosc
+        return "Witaj nazywam się: " + self.imie, "pracuje jako: " + self.specjanosc
 
     def zbieranie(self):
+        print("Potrzebuje kilku roślin, wybierz gdzie mam się udać: "
+              "\n1: Las "
+              "\n2: Bagna")
+        zbieraj = input("Wybierz ___ : ")
+
+        if zbieraj == 1:
+            print("Ide do lasu. ")
+            time.sleep(4)
+        elif zbieraj == 2:
+            print("Ide na bagna. ")
+            time.sleep(6)
         lista_ziol = []
         ziolo_1 = random.randint(0, 1)
         ziolo_2 = random.randint(0, 1)
 
         if ziolo_1 == 1 :
-            lista_ziol.append("Bagienne źiele")
+            lista_ziol.append("Czerwone źiele")
             lista_ziol.append("Niebieski bez")
+            lista_ziol.append("Korzeń piementu")
+            lista_ziol.append("Kwiat drugrotu")
             print("Udało mi sie zebrać następujące rośliny: ", lista_ziol)
         elif ziolo_2 == 1 :
+            lista_ziol.append("Bagienne źiele")
             lista_ziol.append("Kulczyba wronie oko")
             lista_ziol.append("Trójkolorowe źiele")
+            lista_ziol.append("Jaskółcze źiele")
+            lista_ziol.append("Ziarna sporyszu")
             print("Udało mi sie zebrać następujące rośliny: ", lista_ziol)
-        else:
-            print("Nie udało mi sie nic zebrać. Mój plecak jest pusty.")
 
+
+
+    def mikstury(self):
+        miksturki = {1:"",2:"",
+                     3:"",4:"",
+                     5:"",6:""}
+
+        print("Moge ofiarowac ci kilka pomocnych mikstur: "
+              "\n 1. Lęczące: "
+              "\n 2. Regeneracyjne: "
+              "\n 3. Trucizny: ")
+        pokaz_miks = input("Wybierz ___ : ")
+        if pokaz_miks == 1:
+            print()
 
 
 
